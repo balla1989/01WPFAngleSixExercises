18b31b581d2e805b2de12b2ffa8e2788ca48ea30
Suman Balla <suman.balla@wsp.com>
2020-04-12 21:36:15 +1000

    Finished Code Behind.

--- WPFIntro1/WPFIntro1/MainWindow.xaml.cs
+++ WPFIntro1/WPFIntro1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WPFIntro1
 
         private void ApplyButtonName_Click(object sender, RoutedEventArgs e)
         {
-            //to access the button you can do this.//
+            //to access the button you can do this. Basically this ==all the items in WPFIntro1.Mainwindow//
             MessageBox.Show($"The description is: { this.DescriptionText.Text}");
         }
 
@@ -47,14 +47,39 @@ namespace WPFIntro1
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
+
+        {// here CheckBox is the sender
             //this.LengthText.Text += ((CheckBox)sender).Content;
             this.LengthText.Text += (String)((CheckBox)sender).Content+ " ";
         }
 
         private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
+            // here CheckBox is the sender
             this.LengthText.Text = LengthText.Text.Replace((String)((CheckBox)sender).Content,"");
+        }
+
+        private void FinishDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        {
+            if (this.NoteText == null)
+                return;
+            //here combobox is the sender. we then get the SelectedValue. But the selected value is a ComboBoxItem.
+            //So we then get the Content of ComboBoxItem and type cast it to string.
+            this.NoteText.Text =(string) ((ComboBoxItem)((ComboBox)sender).SelectedValue).Content;
+        }
+
+
+        //this piece of code added so that the default text in "Note" is "Painted"//
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FinishDropdown_SelectionChanged(this.FinishDropDown, null);
+        }
+
+        private void SupplierNameText_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            // this.MassText.Text = (string)((TextBox)sender).DataContext;
+            this.MassText.Text = this.SupplierNameText.Text;
         }
     }
 }
